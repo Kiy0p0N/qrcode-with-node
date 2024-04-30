48b9519d8e646697b26779cc2c9179bd66262d98
Felipe <135260228+Kiy0p0N@users.noreply.github.com>
2024-04-30 13:20:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,67 @@
+//press cat
+$("#coding-cat").click(function(){
+	
+	littleCat();
+	mediaColor();
 
-//change theme
-function changeTheme(){
-    let theme = 0;
-    $("#coding-cat").click(function(){
-        setTimeout(function(){
-            if(theme === 0){
-                $("body").removeClass("dark-theme").addClass("light-theme");
-                
-                $("#arrow-left").attr("src", "image/arrow_back-dark.svg");
-                $("#arrow-right").attr("src", "image/arrow_forward-dark.svg");
+})
 
-
-                //change theme svg images
-                setTimeout(function(){
-                    
-                    $("#github").attr("src", "image/github-dark.svg");
-                    $("#instagram").attr("src", "image/instagram-dark.svg");
-                    $("#linkedin").attr("src", "image/linkedin-dark.svg");
-                    $("#whatsapp").attr("src", "image/whatsapp-dark.svg");
-                }, 100);
-
-                theme++;
-
-            } else {
-                $("body").removeClass("light-theme").addClass("dark-theme");
-
-                
-                $("#arrow-left").attr("src", "image/arrow_back-light.svg");
-                $("#arrow-right").attr("src", "image/arrow_forward-light.svg");
-
-                //change theme svg images
-                setTimeout(function(){
-                    
-                    $("#github").attr("src", "image/github-light.svg");
-                    $("#instagram").attr("src", "image/instagram-light.svg");
-                    $("#linkedin").attr("src", "image/linkedin-light.svg");
-                    $("#whatsapp").attr("src", "image/whatsapp-light.svg");
-                },100);
-
-                theme = 0;
-            }
-
-        }, 100);
-    })
-}
-
-changeTheme();
 
 //change photo
-function changePhoto(){
-    let photo = 1;
-    $("#arrow-left").click(function(){
 
-        if(photo >= 2){
-            photo--;
-        } else{
-            photo = 6;
-        }
-        let actualPhotoLeft = "image/share-" + photo + ".jpg";
-        $("#photo img").attr("src", actualPhotoLeft);
-    });
-    $("#arrow-right").click(function(){
+let photo = 1;
+$("#arrow-left").click(function(){
 
-        if(photo <= 5){
-            photo++;
-        } else{
-            photo = 1;
-        }
-        let actualPhotoRight = "image/share-" + photo + ".jpg";
-        $("#photo img").attr("src", actualPhotoRight);
-    });
+	if(photo >= 2){
+        photo--;
+    } else{
+        photo = 6;
+    }
+    let actualPhotoLeft = "image/share-" + photo + ".jpg";
+    $("#photo img").attr("src", actualPhotoLeft);
+});
+    
+$("#arrow-right").click(function(){
+
+    if(photo <= 5){
+        photo++;
+    } else{
+        photo = 1;
+    }
+    let actualPhotoRight = "image/share-" + photo + ".jpg";
+    $("#photo img").attr("src", actualPhotoRight);
+});
+
+
+
+//little cat
+function littleCat(){
+	var catSound = new Audio("image/cat-sound.mp3");
+	catSound.play();
+
+	setTimeout(function(){
+		alert("Por favor, não atrapalhe o gatinho enquanto ele programa!");
+	}, 1000);
 }
-changePhoto();
+
+
+
+
+
+// social media icons color
+function mediaColor(){
+	if($("body").attr("class") === "light-theme"){
+
+		$("#github").attr("src", "image/github-dark.svg");
+        $("#instagram").attr("src", "image/instagram-dark.svg");
+        $("#linkedin").attr("src", "image/linkedin-dark.svg");
+        $("#whatsapp").attr("src", "image/whatsapp-dark.svg");
+
+	} else {
+
+		$("#github").attr("src", "image/github-light.svg");
+        $("#instagram").attr("src", "image/instagram-light.svg");
+        $("#linkedin").attr("src", "image/linkedin-light.svg");
+        $("#whatsapp").attr("src", "image/whatsapp-light.svg");
+	}
+}
